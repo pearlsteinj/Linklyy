bee8c1df706aa912873dc1dc577f1d32fe03abf3
Josh Pearlstein <pearlsteinj@me.com>
2014-09-13 01:26:49 -0400

    changes

---
+++ iOS/Linklyy/Linklyy/LNKAppListViewController.m
@@ -0,0 +1,49 @@
+//
+//  LNKAppListViewController.m
+//  Linklyy
+//
+//  Created by Josh Pearlstein on 9/13/14.
+//  Copyright (c) 2014 Linklyy. All rights reserved.
+//
+
+#import "LNKAppListViewController.h"
+
+@interface LNKAppListViewController ()
+
+@end
+
+@implementation LNKAppListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ iOS/Linklyy/Linklyy/LNKAppSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LNKAppSettingsViewController.h
+//  Linklyy
+//
+//  Created by Josh Pearlstein on 9/13/14.
+//  Copyright (c) 2014 Linklyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LNKAppSettingsViewController : UIViewController
+
+@end
